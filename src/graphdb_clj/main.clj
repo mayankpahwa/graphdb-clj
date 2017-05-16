724e89267d3539bf773331d31c45e46f29aeb361
Mayank <mayankpahwa@gmail.com>
2017-05-16 17:42:45 +0530

    Add delete function to handle delete query   - Added a helper function to delete an individual node and all relationships connected to it

--- src/graphdb_clj/main.clj
+++ src/graphdb_clj/main.clj
@@ -30,3 +30,31 @@
 	(case data
 	    '*' @graph-state
 	    (@graph-state data)))
+
+;;Delete Functions-------------------
+
+(defn helper-delete [node-id]
+	(let [in-nodes (map first ((@graph-state node-id) :out-edge))
+		  out-nodes (map first ((@graph-state node-id) :in-edge))]
+ (map 
+ 	 (fn [n] (swap! graph-state update-in [n :in-edge] (filter 
+                                                       #(not (contains? % node-id)) 
+                                                       ((@graph-state n) :in-edge)))) 
+ 	  in-nodes)
+ (map 
+ 	 (fn [n] (swap! graph-state update-in [n :out-edge] (filter 
+ 		                                              #(not (contains? % node-id)) 
+ 	                                                  ((@graph-state n) :out-edge)))) 
+ 	  out-nodes))
+	(swap! graph-state dissoc node-id))
+
+(defn delete-fn [data]
+	(let [all-nodes (keys @graph-state)
+		  match-dict (data :match)
+		  match-filter-nodes (if (empty? match-dict) 
+                                  all-nodes
+                                  (filter #(= (match-dict :type) (% :type)) all-nodes))
+		  [where-key where-val] (flatten (vec (data :where)))
+		  where-filter-nodes (filter #(= where-val (% where-key)) match-filter-nodes)
+		  ]
+		(map helper-delete where-filter-nodes)))
