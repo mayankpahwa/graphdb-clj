95ec30444503046d47506a0a212b9903cce38147
p4v4n <pavan.barcelona@gmail.com>
2017-05-16 12:55:58 +0530

    Add create-fn to handle create queries.   -added 3 helper functions to add individual node/edge.

--- src/graphdb_clj/main.clj
+++ src/graphdb_clj/main.clj
@@ -3,5 +3,23 @@
 (def graph-state (atom (eval (read-string "src/database/graph1.txt"))))
 
 
+;Create Functions------------------
 
+(defn create-node [node-dict]
+	(swap! graph-state assoc-in (node-dict :id) (assoc (dissoc node-dict :id) :in-edge [] :out-edge [])))
+
+(defn create-edge [[head tail type]]
+	(swap! graph-state update-in [head :out-edge] conj [tail type])
+	(swap! graph-state update-in [tail :in-edge] conj [head type]))
+
+
+(defn helper-create [data-dict]
+	(if (contains? data-dict :node)
+		(create-node (data-dict :node))
+		(create-edge (data-dict :edge))))
+
+(defn create-fn [data]
+	(map helper-create data))
+
+;----------------------------------
 
