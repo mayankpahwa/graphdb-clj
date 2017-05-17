627d1290db87488acf1c0a869a1b66f0df837b4a
Mayank <mayankpahwa@gmail.com>
2017-05-17 10:23:39 +0530

    Fix an issue with the delete-fn and refactor helper-delete function   - Add 2 separate functions for deleting in and out relationships

--- src/graphdb_clj/main.clj
+++ src/graphdb_clj/main.clj
@@ -33,28 +33,27 @@
 
 ;;Delete Functions-------------------
 
+(defn delete-in-edge [in-node node-id]
+    (let [updated-edge-list (filter #(not (contains? % node-id)) ((@graph-state in-node) :in-edge))]
+        (swap! graph-state assoc-in [in-node :in-edge] updated-edge-list)))
+
+(defn delete-out-edge [out-node node-id]
+    (let [updated-edge-list (filter #(not (contains? % node-id)) ((@graph-state out-node) :out-edge))]
+        (swap! graph-state assoc-in [out-node :out-edge] updated-edge-list)))
+
 (defn helper-delete [node-id]
-	(let [in-nodes (map first ((@graph-state node-id) :out-edge))
-		  out-nodes (map first ((@graph-state node-id) :in-edge))]
- (map 
- 	 (fn [n] (swap! graph-state update-in [n :in-edge] (filter 
-                                                       #(not (contains? % node-id)) 
-                                                       ((@graph-state n) :in-edge)))) 
- 	  in-nodes)
- (map 
- 	 (fn [n] (swap! graph-state update-in [n :out-edge] (filter 
- 		                                              #(not (contains? % node-id)) 
- 	                                                  ((@graph-state n) :out-edge)))) 
- 	  out-nodes))
-	(swap! graph-state dissoc node-id))
+    (let [in-nodes (map first ((@graph-state node-id) :out-edge))
+          out-nodes (map first ((@graph-state node-id) :in-edge))]
+     (map #(delete-in-edge % node-id) in-nodes)
+     (map #(delete-out-edge % node-id) out-nodes)
+     (swap! graph-state dissoc node-id)))
 
 (defn delete-fn [data]
-	(let [all-nodes (keys @graph-state)
-		  match-dict (data :match)
-		  match-filter-nodes (if (empty? match-dict) 
+    (let [all-nodes (keys @graph-state)
+          match-dict (data :match)
+          match-filter-nodes (if (empty? match-dict)
                                   all-nodes
-                                  (filter #(= (match-dict :type) (% :type)) all-nodes))
-		  [where-key where-val] (flatten (vec (data :where)))
-		  where-filter-nodes (filter #(= where-val (% where-key)) match-filter-nodes)
-		  ]
-		(map helper-delete where-filter-nodes)))
+                                  (filter #(= (match-dict :type) ((@graph-state %) :type)) all-nodes))
+          [where-key where-val] (flatten (vec (data :where)))
+          where-filter-nodes (filter #(= where-val ((@graph-state %) where-key)) match-filter-nodes)]
+    (map helper-delete where-filter-nodes)))
