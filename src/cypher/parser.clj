3e29d779c89993eed296af321457a6a17b1100b9
Mayank <mayankpahwa@gmail.com>
2017-05-21 16:04:44 +0530

    Add individual regex patterns for all parsers

--- src/cypher/parser.clj
+++ src/cypher/parser.clj
@@ -2,6 +2,22 @@
 (ns cypher.parser)
 (use '[clojure.string])
 
+;; Individual regular expression patterns
+(def create-node-regex #"^\([a-zA-Z]\w*\s*:\s*[a-zA-Z]\w*\s*(\{([a-zA-Z]\w*:(('\w+')|([0-9]+)),)*(\w+:(('\w+')|([0-9]+)))\})?\)")
+
+(def create-edge-regex #"^\([a-zA-Z]\w*\)-\[:[a-zA-Z]\w*\]->\([a-zA-Z]\w*\)")
+
+(def match-condition-regex #"^\([a-zA-Z]\w*(:[a-zA-Z]\w*)?\)")
+
+(def property-fix-regex #"^[a-zA-Z]\w*\.[a-zA-Z]\w*\s*=\s*(('\w+')|([0-9]+))")
+
+(def property-query-regex #"^[a-zA-Z]\w*\.[a-zA-Z]\w*")
+
+(def type-regex #"^[a-zA-Z]\w*:[a-zA-Z]\w*")
+
+(def id-regex #"^[a-zA-Z]\w*")
+
+
 (defn any-one-parser-factory [& args]
 	(fn [data] ((reduce #(if (%1 data) %1 %2) args) data)))
 
@@ -9,6 +25,3 @@
   (let [keywords ["create " "match " "where " "return " "delete " "remove " "set "]]
     (if (not-empty (filter #(starts-with? query %) keywords))
     	(split query #"\s+" 2))))
-
-
-
