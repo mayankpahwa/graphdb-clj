c4fd062892f4e2d3e3bbe157c2b26c3241ff2ded
p4v4n <pavan.barcelona@gmail.com>
2017-05-22 17:57:18 +0530

    Add functions to build dictionary from the query string.   -node-edge-builder redirects to node/edge builder based on query-string-block type.

--- src/cypher/interpreter.clj
+++ src/cypher/interpreter.clj
@@ -1,5 +1,18 @@
 ;;Interpreter for the cypher query language
 
+(defn node-builder [q-string]
+	(hashmap :node (node-parse-build-dict q-string)))
+
+(defn edge-builder [q-string]
+	(hashmap :edge (edge-parse-build-list q-string)))
+
+(defn node-edge-builder [q-string]
+	(if (.contains q-string "{")
+		(node-builder q-string)
+		(edge-builder q-string)))
+
+(defn create-dict-builder [q-list]
+	(hashmap :create (vec (map node-edge-builder q-list))))
 
 (defn query-interpreter [query-list]
 	(case (first query-list)
