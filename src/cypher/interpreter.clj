cb2ab4e2c652980e08f34398b5aaff42eed578a4
Mayank <mayankpahwa@gmail.com>
2017-05-23 16:49:46 +0530

    Fix minor issues with sytax   -hashmap turned into hash-map   -def turned to defn in function definitions   -regex turned to string while using starts-with? function   -add driver function for testing

--- src/cypher/interpreter.clj
+++ src/cypher/interpreter.clj
@@ -2,12 +2,7 @@
 (ns cypher.interpreter)
 (use '[clojure.string])
 
-n.property1 = value1
-n:Person
-n
-n.property,n:type
-
-(def property-fix-regex #"^\w+\.(\w+)\s*=\s*(.+)$"
+(def property-fix-regex #"^\w+\.(\w+)\s*=\s*(.+)$")
 
 (def property-query-regex #"^\w+\.(\w+)$")
 
@@ -15,32 +10,32 @@ n.property,n:type
 
 (def id-regex #"^\w+$")
 
-(def change-type [value-str]
-  (if (starts-with? value-str #"'") 
+(defn change-type [value-str]
+  (if (starts-with? value-str "'") 
 	  (replace value-str #"'" "") 
 	  (Integer. value-str)))
 
-(def keyword-ify [[key-str value-str]]
+(defn keyword-ify [[key-str value-str]]
 	[(keyword key-str) (change-type value-str)])
 
 (defn json-to-map-fn [json-str]
 	(let [json-list (map trim (split json-str #","))
 		  key-val-pair-list (map #(map trim (split % #":")) json-list)]
-		 (map keyword-ify key-val-pair-list)))
+          (map keyword-ify key-val-pair-list)))
 
 (defn node-parse-build-dict [q-string]
 	(let [[full-match id-val type-val json-str] (re-find #"\((\w+):(\w+)\s*\{(.+)\}\)" q-string)]
 		(into (hash-map :id id-val :type type-val) (json-to-map-fn json-str))))
 
 (defn node-builder [q-string]
-	(hashmap :node (node-parse-build-dict q-string)))
+	(hash-map :node (node-parse-build-dict q-string)))
 
 (defn edge-parse-build-list [q-string]
   (let [[full-match head type tail] (re-find #"\((\w+)\)-\[:(\w+)\]->\((\w+)\)" q-string)]
   	[head tail type]))
 
 (defn edge-builder [q-string]
-	(hashmap :edge (edge-parse-build-list q-string)))
+	(hash-map :edge (edge-parse-build-list q-string)))
 
 (defn node-edge-builder [q-string]
 	(if (.contains q-string "{")
@@ -48,7 +43,7 @@ n.property,n:type
 		(edge-builder q-string)))
 
 (defn create-dict-builder [q-list]
-	(hashmap :create (vec (map node-edge-builder q-list))))
+	(hash-map :create (vec (map node-edge-builder q-list))))
 
 (defn type-identify [data]
 	(let [type-match (re-find #"\(\w+\s*:\s*(\w+)\)" data)]
@@ -93,3 +88,7 @@ n.property,n:type
 	  "create" (create-dict-builder (rest query-list))
 	  "match" (match-dict-builder (rest query-list))
      (prn "Invalid Syntax")))
+
+(defn driver []
+    (let [query-string (read-line)]
+      (prn (query-interpreter (read-string query-string)))))
