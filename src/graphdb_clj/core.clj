25985611d4fc68485aa394f2f14787c3a0dd4032
p4v4n <pavan.barcelona@gmail.com>
2017-05-24 11:05:50 +0530

    Add the handler function to handle user-queries.   -Include namespaces of parser,interpreter,main files.

--- src/graphdb_clj/core.clj
+++ src/graphdb_clj/core.clj
@@ -1,6 +1,17 @@
 (ns graphdb-clj.core
-  (:use [graphdb-clj.main])
+  (:require [cypher.parser :as parser]
+  	        [cypher.interpreter :as interpreter]
+  	        [graphdb-clj.main :as main])
   (:gen-class))
+
+(defn handler []
+    (let [query-str (read-line)
+    	  query-list (parser/query-parser query-str)
+          query-dict (interpreter/query-interpreter query-list)
+          query-key (first (keys query-dict))
+          query-value (first (vals query-dict))]
+      ((main/MATCH-QUERY query-key) query-value))
+  (main/save-to-file))
 
 (defn -main
   []
